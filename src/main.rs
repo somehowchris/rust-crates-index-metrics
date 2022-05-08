9573063ce27f5def575b68a730f60713e7dcfb42
Christof Weickhardt <christof@weickhardt.ch>
2022-05-08 10:25:39 +0000

    feat: progress bar

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,25 @@
+#[macro_use]
+extern crate tracing;
+
 use rayon::prelude::*;
 use std::borrow::Cow;
 
-use std::{collections::HashMap, sync::Arc};
+use std::collections::HashSet;
+use std::sync::Arc;
 use tokio::sync::Semaphore;
 use tokio_util::io::StreamReader;
 
+use indicatif::{ProgressBar, ProgressStyle};
+use std::sync::RwLock;
+
+use async_std::path::Path;
 use async_tar::Archive;
 use cargo_toml::Manifest;
 use crates_index::{Crate, Version};
 use futures::io::AsyncReadExt;
 use futures::TryStreamExt;
 use futures_util::StreamExt;
-use async_std::path::Path;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 mod binstall;
 
@@ -54,169 +62,145 @@ pub struct Metric {
 
 #[tokio::main]
 async fn main() {
-    let mut index = crates_index::Index::new_cargo_default().unwrap();
-    println!("Updating index");
-    index.update().unwrap();
-    let mut crates_with_binstall_support = HashMap::<String, usize>::new();
-    let mut versions_not_supporting_https = HashMap::<(String, String), String>::new();
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::EnvFilter::new(
+            std::env::var("RUST_LOG").unwrap_or_else(|_| "info".into()),
+        ))
+        .with(tracing_subscriber::fmt::layer())
+        .init();
 
-    println!("Counting index");
-    let creates_count = index.crates_parallel().count();
+    let mut index = crates_index::Index::new_cargo_default().unwrap();
+
+    info!("Updating index");
+    index.update().unwrap();
 
     let config = index.index_config().unwrap();
 
-    let data = index
-        .crates()
-        .enumerate()
-        .map(|(c, single_crate)| {
-            let config = config.clone();
-            async move {
-                if c % 500 == 0 {
-                    println!(
-                        "crate name: {}   {}/{}\t\t",
-                        single_crate.name(),
-                        c,
-                        creates_count
-                    );
-                }
-                let futs = single_crate
-                    .versions()
-                    .par_iter()
-                    .filter_map(|version| {
-                        if version.is_yanked() {
-                            return None;
-                        }
-
-                        let crate_url = version.download_url(&config).unwrap();
-
-                        Some(async move {
-                            let url = &crate_url.clone();
-                            let resp = backoff::future::retry(
-                                backoff::ExponentialBackoff::default(),
-                                || async move { Ok(reqwest::get(url).await?) },
-                            )
-                            .await
-                            .unwrap();
-
-                            if !resp.status().is_success() {
-                                return None;
-                            }
-
-                            let tgz = Archive::new(
-                                tokio_util::io::ReaderStream::new(
-                                    async_compression::tokio::bufread::GzipDecoder::new(
-                                        StreamReader::new(resp.bytes_stream().map_err(|e| {
-                                            std::io::Error::new(std::io::ErrorKind::Other, e)
-                                        })),
-                                    ),
-                                )
-                                .into_async_read(),
-                            );
-
-                            let mut entries = tgz.entries().unwrap();
-
-                            let crate_file =
-                                format!("{}-{}/Cargo.toml", version.name(), version.version());
-
-                            let path = Cow::from(Path::new(&crate_file));
-
-                            let mut buff = None;
-
-                            while let Some(file) = entries.next().await {
-                                if let Ok(mut value) = file {
-                                    if let Ok(file_path) = value.path() {
-                                        if path == file_path {
-                                            let mut buffer = vec![];
-
-                                            value.read_to_end(&mut buffer).await.unwrap();
-                                            buff = Some(buffer);
-
-                                            break;
-                                        }
-                                    }
-                                }
-                            }
-
-                            let metrics = BinstallMetrics::new(buff.unwrap());
-
-                            Some((metrics, version))
-                        })
-                    })
-                    .collect::<Vec<_>>();
-
-                let mut metrics = vec![];
-
-                for fut in futs {
-                    metrics.push(fut.await);
-                }
-
-                (
-                    (
-                        single_crate.name().to_string(),
-                        metrics
-                            .iter()
-                            .filter_map(|e: &Option<(BinstallMetrics, &Version)>| {
-                                if let Some(v) = e {
-                                    if v.0.has_binstall_metadata {
-                                        Some(v.0.has_binstall_metadata)
-                                    } else {
-                                        None
-                                    }
-                                } else {
-                                    None
-                                }
-                            })
-                            .count(),
-                    ),
-                    metrics
-                        .iter()
-                        .filter_map(|metric| {
-                            if let Some(m) = metric {
-                                if m.0.has_binstall_metadata && !m.0.uses_https {
-                                    Some((
-                                        (
-                                            single_crate.to_owned().name().to_string(),
-                                            m.1.to_owned().version().to_string(),
-                                        ),
-                                        m.1.download_url(&config).unwrap(),
-                                    ))
-                                } else {
-                                    None
-                                }
-                            } else {
-                                None
-                            }
-                        })
-                        .collect::<Vec<_>>(),
-                )
-            }
+    let versions = index
+        .crates_parallel()
+        .filter_map(|e| e.ok())
+        .flat_map(|e| {
+            e.versions()
+                .iter()
+                .filter(|v| !v.is_yanked())
+                .cloned()
+                .collect::<Vec<_>>()
         })
         .collect::<Vec<_>>();
 
-    let mut futs = vec![];
+    let total_versions = versions.len();
+
+    let data = versions.into_iter().map(|version| {
+        let config = config.clone();
+
+        let crate_url = version.download_url(&config).unwrap();
+
+        async move {
+            let url = &crate_url.clone();
+            let resp =
+                backoff::future::retry(backoff::ExponentialBackoff::default(), || async move {
+                    Ok(reqwest::get(url).await?)
+                })
+                .await
+                .unwrap();
+
+            if !resp.status().is_success() {
+                return None;
+            }
+
+            let tgz = Archive::new(
+                tokio_util::io::ReaderStream::new(
+                    async_compression::tokio::bufread::GzipDecoder::new(StreamReader::new(
+                        resp.bytes_stream()
+                            .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e)),
+                    )),
+                )
+                .into_async_read(),
+            );
+
+            let mut entries = tgz.entries().unwrap();
+
+            let crate_file = format!("{}-{}/Cargo.toml", version.name(), version.version());
+
+            let path = Cow::from(Path::new(&crate_file));
+
+            let mut buff = None;
+
+            while let Some(file) = entries.next().await {
+                if let Ok(mut value) = file {
+                    if let Ok(file_path) = value.path() {
+                        if path == file_path {
+                            let mut buffer = vec![];
+
+                            value.read_to_end(&mut buffer).await.unwrap();
+                            buff = Some(buffer);
+
+                            break;
+                        }
+                    }
+                }
+            }
+
+            if let Some(buffer) = buff {
+                let metrics = BinstallMetrics::new(buffer);
+
+                Some((metrics, version))
+            } else {
+                None
+            }
+        }
+    });
+
     let semaphore = Arc::new(Semaphore::new(num_cpus::get() * 8));
+    let bar = Arc::new(ProgressBar::new(total_versions.try_into().unwrap()));
+
+    bar.set_style(
+        ProgressStyle::default_bar()
+            .template("[{elapsed_precise}] {bar:40.cyan/blue} {pos:>7}/{len:7} {msg}")
+            .progress_chars("##-"),
+    );
+
+    let results = Arc::new(RwLock::new(
+        Vec::<(BinstallMetrics, Version)>::with_capacity(total_versions),
+    ));
 
     for item in data {
         let permit = semaphore.clone().acquire_owned().await;
-        futs.push(tokio::spawn(async move {
+        let progress_bar = Arc::clone(&bar);
+        let results = Arc::clone(&results);
+        tokio::spawn(async move {
             let data = item.await;
+
+            progress_bar.inc(1);
             drop(permit);
-            data
-        }));
+            if let Some((metrics, version)) = data {
+                let mut result = results.write().unwrap();
+                result.push((metrics, version));
+            }
+        });
     }
 
-    for fin in futs {
-        if let Ok((install_meta_data, https_use)) = fin.await {
-            crates_with_binstall_support.insert(install_meta_data.0, install_meta_data.1);
-            if install_meta_data.1 > 0 {
-                println!("+1");
-            }
-            for (key, value) in https_use {
-                versions_not_supporting_https.insert(key, value);
-            }
-        }
-    }
+    let data = results.read().unwrap();
 
-    println!("{:?}", crates_with_binstall_support.keys());
-    println!("{:?}", versions_not_supporting_https);
+    let binstall_data = data
+        .par_iter()
+        .filter(|e| e.0.has_binstall_metadata)
+        .collect::<Vec<_>>();
+
+    info!("versions with binstall support: {:?}", binstall_data.len());
+    info!(
+        "crates with binstall support: {:?}",
+        binstall_data
+            .par_iter()
+            .map(|(_metric, version)| version.name())
+            .collect::<HashSet<_>>()
+            .len()
+    );
+    info!(
+        "names of crates with binstall support: {:?}",
+        binstall_data
+            .par_iter()
+            .map(|(_metric, version)| version.name())
+            .collect::<HashSet<_>>()
+    );
 }
